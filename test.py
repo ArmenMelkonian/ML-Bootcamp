8a468440114358053a86c452d43e03a5913f4773
Armen Melkonyan <armenmelkonyan223@gmail.com>
2021-10-10 15:07:46 +0400

    After PyCharm

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ print(1+7)
 """
     First file
 """
+# print(int("56") + 0.5)
+# x = eval(input("Enter a random number: "))
+# print("Square is ",x ** 2)
+print("s" in "sol")
+x = 7
+s = "This is string"
+l = s.split()
+print(l)
+print(" 1 ".join(l))
+print(f"This is {x = }")
